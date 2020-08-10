c86295db9624ef47effd557814d318a9e61af667
Becca <rebecca.schedler@gmx.net>
2020-08-10 21:36:40 +0200

    test upload

---
+++ aboutme.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>LaGomba</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
+</head>
+<body>
+
+  
+  
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" routerLink="/"><img src="/img/LaGomba-logo-sm.png" alt="lagomba-logo"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item" routerLinkActive='active' [routerLinkActiveOptions]="{exact: true}">
+        <a class="nav-link" routerLink='/'>Home</a>
+      </li>
+      <li class="nav-item" routerLinkActive='active'>
+        <a class="nav-link" routerLink="/about-mushrooms">About Me</a>
+      </li>
+      <li class="nav-item" routerLinkActive='active'>
+        <a class="nav-link" routerLink='/about-us'>Recipes</a>
+      </li>
+      <li class="nav-item" routerLinkActive='active'>
+        <a class="nav-link" routerLink='/contact'>Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+<div class="container">
+  <h1>WHO IS NORA?</h1>
+  <p>Nora decided to act on her Climate Grief and started looking for solutions that are doing good for the environment and somewhat profitable. She wanted to help lowering meat consumption and at the same time clean the environment. Months of research has started and finally the answer was: A Vienna based company called Hut&Stiel that recycles coffee waste and turns it into delicious Oyster mushroom. Nora started her internship at the company right away.</p>
+  <h3>HOW IT ALL STARTED?</h3>
+  <p>In the 3rd district of Vienna in Markhofgasse there is a coworking space full of vibrant people with life and energy. Apart from the many people there are many spaces also. At the end of 2019 in one of the basements that is not too big and not too small renewal working has started. Viennese people like to drink coffee. They like to drink coffee a lot and there are thousands of kilograms of coffee grounds ending up in the trash day by day.</p>
+  <h3>HOW DOES IT WORK?</h3>
+  <p>Instead of going to the garbage the daily collected coffee grounds go to Hut & Stiel's production site where they are carefully turned in to mushroom substrate. The substrate goes in to bags and the bags go in to the incubational room. After 3 weeks of incubating they go to Nora's basement to fruit and to be harvested.</p>
+  <h3>TIME TO START!</h3>
+  <p>After months of hammering, disassembling, assembling, painting, cleaning, drilling, cleaning, fixing and then cleaning the basement started to appear like a 70 square meters mushroom farm.</p>
+  <h3>THE FIRST BAGS HAS ARRIVED!</h3>
+  <p>Nora started the production with 50 bags of incubated oyster mushroom (Pleurotus ostreatus) and 4 bags of Lion's Mane (Hericium erinaceus) that are from a Hungarian mushroom company. LaGomba Urban Mushroom</p>
+  <h3>ARE YOU INTERESTED?</h3>
+  <p>If you would like to order fresh urban farmed eco-friendly Oyster Musrhoom, please contact Nora. email: rr.noar@gmail.com LaGomba Urban Mushroom</p>
+  
+
+
+</div>
+
+<!-- Footer -->
+<footer class="page-footer font-small .bg-light pt-4">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h5 class="text-uppercase">Footer</h5>
+        <p>Mushroom Footer</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Links</h5>
+
+       
+
+      </div>
+      <!-- Grid column -->
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright: La Gomba
+    
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
